a05da39dd6a3e1f77b580c1eba01ce0b9f9e26e9
Jordan Bolton <jordanbolton1999@gmail.com>
2018-03-16 09:38:45 +0000

    album and track

--- nmeAssessment/src/main/java/nme/nmeassessment/domain/Album.java
+++ nmeAssessment/src/main/java/nme/nmeassessment/domain/Album.java
@@ -5,10 +5,69 @@
  */
 package nme.nmeassessment.domain;
 
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  *
  * @author Chris
  */
+
+@Entity
 public class Album {
+    @Id
+    @Column(name="albumId")
+    private int albumID;
+    
+    private String albumTitle;
+    private Date releaseDate;
+    @Column(name="artistId")
+    private int artistID;
+    
+    public Album() {
+        
+    }
+
+    public int getAlbumID() {
+        return albumID;
+    }
+
+    public void setAlbumID(int albumID) {
+        this.albumID = albumID;
+    }
+
+    public String getAlbumTitle() {
+        return albumTitle;
+    }
+
+    public void setAlbumTitle(String albumTitle) {
+        this.albumTitle = albumTitle;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getArtistID() {
+        return artistID;
+    }
+
+    public void setArtistID(int artistID) {
+        this.artistID = artistID;
+    }
+
+    @Override
+    public String toString() {
+        return "Album{" + "albumID=" + albumID + ", albumTitle=" + albumTitle + ", releaseDate=" + releaseDate + ", artistID=" + artistID + '}';
+    }
+    
+    
+    
     
 }
