38d1c95fa53720fb8c1c99256b3a41b52f0a6960
ChrisSofokleous <christophersofokleous@gmail.com>
2018-03-16 10:11:20 +0000

    Added implementation for all findAll methods. Still working on returning the count (NOT WORKING)

--- nmeAssessment/src/main/java/nme/nmeassessment/dao/NmeRepositoryJPA.java
+++ nmeAssessment/src/main/java/nme/nmeassessment/dao/NmeRepositoryJPA.java
@@ -5,9 +5,15 @@
  */
 package nme.nmeassessment.dao;
 
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import nme.nmeassessment.domain.Album;
+import nme.nmeassessment.domain.Artist;
+import nme.nmeassessment.domain.Track;
 
 /**
  *
@@ -18,6 +24,27 @@ public class NmeRepositoryJPA implements NmeRepository {
     
     @PersistenceContext
     private EntityManager em;
+    
+    
+    public List<Artist> finsAllArtists() {
+        TypedQuery<Artist>query = em.createQuery("SELECT a FROM Artist as a", Artist.class);
+        return query.getResultList();
+    }
+    
+    public List<Track> findAllTracks() {
+        TypedQuery<Track>query = em.createQuery("SELECT t FROM Track as t", Track.class);
+        return query.getResultList();
+    }
+    
+    public List<Album> findAllAlbums() {
+        TypedQuery<Album>query = em.createQuery("SELECT a FROM Album as a", Album.class);
+        return query.getResultList();
+    }
+    
+    public int artistCount() {
+        Query query = em.createQuery("SELECT COUNT(*) from Artist");
+        return query.getSingleResult();
+    }
     
     
     
