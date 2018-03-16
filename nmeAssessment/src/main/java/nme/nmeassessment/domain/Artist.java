87a7aada1d431ae6f41bf65606aed6ae0f60bd98
ChrisSofokleous <christophersofokleous@gmail.com>
2018-03-16 09:38:08 +0000

    Built the Artist class

--- nmeAssessment/src/main/java/nme/nmeassessment/domain/Artist.java
+++ nmeAssessment/src/main/java/nme/nmeassessment/domain/Artist.java
@@ -5,10 +5,63 @@
  */
 package nme.nmeassessment.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+
 /**
  *
  * @author Chris
  */
 public class Artist {
+    
+    @Id
+    @Column(name = "artistId")
+    private int artistID;
+    private String artistName;
+    private String genre;
+    private int sales;
+    
+    public Artist() {
+        
+    }
+
+    public int getArtistID() {
+        return artistID;
+    }
+
+    public void setArtistID(int artistID) {
+        this.artistID = artistID;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getSales() {
+        return sales;
+    }
+
+    public void setSales(int sales) {
+        this.sales = sales;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" + "artistID=" + artistID + ", artistName=" + artistName + ", genre=" + genre + ", sales=" + sales + '}';
+    }
+    
+    
     
 }
